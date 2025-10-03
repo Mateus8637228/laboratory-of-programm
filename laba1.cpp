1e0c214aa619125f599b35db1d7a8ba73bdb1c90
Mateus8637228 <matrac2k20@gmail.com>
2025-10-03 08:33:21 +0700

    laba1.cpp

--- laba1.cpp
+++ laba1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
   const double PI = 3.1415926535;
   
-  double a, h, d, delta, h0, mu, g;
+  double a, h, d, delta, h0, mu, g, f, F0, F, K, t;
   
   cout << "a = "; cin >> a;
   cout << "h = "; cin >> h;
@@ -20,16 +20,10 @@ int main() {
   cout << "mu = "; cin >> mu;
   cout << "g = "; cin >> g;
 
-  double f
   f = PI * d * d / 4.0;
-  double F0
   F0 = a * a;
-  double F
   F = (a - 2.0 * delta) * (a - 2.0 * delta);
-
-  double K
   K = sqrt(h0 - delta) - sqrt(h - delta - (h - h0) * (F0 / F));
-  double t
   t = (2.0 * K) / (mu * f * sqrt(2.0 * g)) * (F0 * F) / (F0 - F);
 
   cout << "t = " << t << endl;
